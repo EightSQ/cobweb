73ecf7c369fef552a06486c8ca79e2b7cc092afc
Otto Kissig <1622854+EightSQ@users.noreply.github.com>
2023-04-01 22:20:56 +0200

    Add more advanced test case

--- test/unit/timer_queue.cpp
+++ test/unit/timer_queue.cpp
@@ -1,4 +1,7 @@
+#include <algorithm>
+#include <random>
 #include <utility>
+#include <vector>
 
 #include "cobweb/cobweb.hpp"
 #include "gmock/gmock.h"
@@ -39,6 +42,50 @@ TEST_F(TimerQueueTest, Basic) {
   EXPECT_THAT(tq.poll(5), Optional(1339));
 
   EXPECT_TRUE(tq.empty());
+}
+
+TEST_F(TimerQueueTest, Advanced) {
+  constexpr int K = 8;
+  TimerQueue<uint64_t, K> tq;
+  std::mt19937 gen(42);
+  std::uniform_int_distribution<uint64_t> val_dist;
+  std::uniform_int_distribution<uint64_t> start_time_test(1, (1u << 16));
+  std::uniform_int_distribution<uint64_t> ttl_bit_dist(1, K - 1);
+  std::vector<std::pair<std::pair<uint64_t, int>, uint64_t>>
+      inputs; // ((starttime, ttl), value)
+  std::vector<std::pair<std::pair<uint64_t, int>, uint64_t>>
+      expected_outputs; // ((deadline, ttl), value)
+
+  constexpr int num_objects = (1 << 16);
+  for (int i = 0; i < num_objects; ++i) {
+    uint64_t start_time = start_time_test(gen);
+    uint64_t ttl = (1ull << ttl_bit_dist(gen));
+    uint64_t deadline_time = start_time + ttl;
+    uint64_t value = val_dist(gen);
+    inputs.push_back({{start_time, ttl}, value});
+    expected_outputs.push_back({{start_time + ttl, ttl}, value});
+  }
+
+  std::sort(inputs.begin(), inputs.end());
+  std::sort(expected_outputs.begin(), expected_outputs.end());
+
+  int input_index = 0;
+  int output_index = 0;
+  for (uint64_t ts = 0; ts <= expected_outputs.back().first.first; ++ts) {
+    while (input_index < num_objects && inputs[input_index].first.first == ts) {
+      const auto &next_input = inputs[input_index++];
+      tq.insert(next_input.second, next_input.first.second, ts);
+    }
+    while (output_index < num_objects &&
+           expected_outputs[output_index].first.first == ts) {
+      const auto &next_output = expected_outputs[output_index++];
+      EXPECT_THAT(tq.poll(ts), Optional(next_output.second));
+    }
+    EXPECT_EQ(tq.poll(ts), std::nullopt);
+  }
+
+  EXPECT_TRUE(tq.empty());
+  EXPECT_EQ(tq.poll(), std::nullopt);
 }
 
 TEST_F(TimerQueueTest, TTLPruning) {
